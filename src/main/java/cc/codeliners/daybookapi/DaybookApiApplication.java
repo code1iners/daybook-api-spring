9be497d82ab79d007766042fc4998b0193a94b72
guswl9814 <guswl9814@fixelsoft.com>
2022-07-27 11:46:01 +0900

    프로젝트 생성

---
+++ src/main/java/cc/codeliners/daybookapi/DaybookApiApplication.java
@@ -0,0 +1,13 @@
+package cc.codeliners.daybookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaybookApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaybookApiApplication.class, args);
+    }
+
+}

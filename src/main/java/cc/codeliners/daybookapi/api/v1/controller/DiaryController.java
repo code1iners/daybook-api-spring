b87d6952048c5ca55a920a61911827e769b6db3b
201511027-se <guswl9814@gmail.com>
2022-08-06 19:47:31 +0900

    @CrossOrigin(origins = *, allowedHeaders = *)

--- src/main/java/cc/codeliners/daybookapi/api/v1/controller/DiaryController.java
+++ src/main/java/cc/codeliners/daybookapi/api/v1/controller/DiaryController.java
@@ -2,12 +2,14 @@ package cc.codeliners.daybookapi.api.v1.controller;
 
 import cc.codeliners.daybookapi.api.v1.common.ApiResponse;
 import cc.codeliners.daybookapi.api.v1.service.DiaryService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class DiaryController {
 
     private final DiaryService diaryService;

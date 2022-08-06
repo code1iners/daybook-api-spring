b3d78e6c399c9c55143884b663a58018e84e71b0
201511027-se <guswl9814@gmail.com>
2022-08-06 18:51:22 +0900

    diaryMain api ApiResponse 변경

--- src/main/java/cc/codeliners/daybookapi/service/DiaryService.java
+++ src/main/java/cc/codeliners/daybookapi/service/DiaryService.java
@@ -3,7 +3,9 @@ package cc.codeliners.daybookapi.service;
 import cc.codeliners.daybookapi.common.ApiResponse;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -14,12 +16,19 @@ public class DiaryService {
 
         result.put("year",2022);
         result.put("month",8);
-        Map data = new HashMap<Integer, Integer>();
+        List diary = new ArrayList();
+
         for (int i=1;i<32;i++){
-            data.put(i, (int)((Math.random()*10000)%10));
+            Map data = new HashMap<String, Integer>();
+
+            data.put("day",i);
+            data.put("count",(int)((Math.random()*10000)%10));
+
+            diary.add(data);
+
         }
 
-        result.put("diaries",data);
+        result.put("diaries",diary);
         return new ApiResponse(200,result);
 
     }
